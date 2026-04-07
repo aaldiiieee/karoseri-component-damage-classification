a0d99ef3e176f672248d1eed386ae05c43c8a301
aaldiiieee <prmdy02@gmail.com>
2026-04-07 07:42:05 +0700

    feat: Update CORS configuration to load origins from environment variable

--- app/configs/cors_config.py
+++ app/configs/cors_config.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-CORS_ORIGINS = [
-    "http://localhost:5173",
-]
+origins_str = os.getenv("CORS_ORIGINS", "http://localhost:5173")
+CORS_ORIGINS = [origin.strip() for origin in origins_str.split(",")]
 
 CORS_CONFIG = {
     "allow_origins": CORS_ORIGINS,
